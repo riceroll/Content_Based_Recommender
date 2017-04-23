200691b933df92fb2174cd5fdaa24a6d6bd96e18
riceroll <584455235@qq.com>
2017-04-23 22:59:39 +0800

    annotation

--- CB_Recommender.py
+++ CB_Recommender.py
@@ -4,12 +4,22 @@ import csv
 import re
 import nltk
 
+# Parameters =====================================
+# the paper to be recommended for
 try:
-	paper_ID = sys.argv[1]
+	paper_ID = int(sys.argv[1])
 except:
 	paper_ID=1
+# all the papers to be recommended from
+file_papers="papers_NLP.csv"
 
-cur=[]
+# Variables ======================================
+# store the paperIDs and abstracts
+PaperID=[]
+abstract=[]
+size = 0
+# get data from the csv
+cur=[] 		
 with open("papers_NLP.csv","rb") as f:
 	reader=csv.reader(f)
 	for row in reader:
@@ -17,17 +27,15 @@ with open("papers_NLP.csv","rb") as f:
 del(cur[0])
 tuple(cur)
 
-PaperID=[]
-abstract=[]
-size = 0
-
+# Text Filtering ==================================
+# filter html & formula
 regex1 = re.compile("<math>.*?</math>")
 regex2 = re.compile("<url>.*?</url>")
 regex3 = re.compile("<.*?>")
 regex4 = re.compile("&.*?;")
 regex5 = re.compile("/.*?>")
 regex6 = re.compile("i>")
-
+# read the file into variables
 i=0
 for row in cur:
 	if len(row)>1:
@@ -39,36 +47,30 @@ for row in cur:
 		sub4=re.sub(regex4,'',sub3)
 		sub5=re.sub(regex5,'',sub4)
 		sub = re.sub(regex6,'',sub5)
-		
 		abstract.append(sub)
 		size += 1
 print size
-
+# lower the text
 text_lower = [[word for word in document.lower().split()]for document in abstract]
-
-
+# tokenize the text
 from nltk.tokenize import word_tokenize                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
 texts_tokenized = [[word.lower() for word in word_tokenize(document.decode(encoding='UTF-8',errors='ignore'))]for document in abstract]
-
-
+# filter stop words
 from nltk.corpus import stopwords
 english_stopwords = stopwords.words('english')
-
-
 texts_filtered_stopwords = [[word for word in document if not word in english_stopwords]for document in texts_tokenized]
-
-
+# filter punctuations
 english_punctuations = [',','.',':',';','?','(',')','[',']','&','!','*','@','#','$','%','...','<','>','^','{','}','\\','/']
 texts_filtered = [[word for word in document if not word in english_punctuations]for document in texts_filtered_stopwords]
 print "Fliter Finished"
 
+# Text stemming ===============================================================================
 from nltk.stem.lancaster import LancasterStemmer
 st= LancasterStemmer()
-
-
 texts = [[st.stem(word)for word in document]for document in texts_filtered]
 print "Stemming Finished"
 
+# Content-based calculation ===================================================================
 from gensim import corpora, models, similarities
 import logging
 logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',level=logging.INFO)
@@ -88,18 +90,16 @@ lsi = models.LsiModel(corpus_tfidf,id2word=dictionary,num_topics=30)
 #Transforming corpus to LSI space and index it to initial the query structure
 index = similarities.MatrixSimilarity(lsi[corpus])
 
+# Commend papers for a specific paper ==========================================================
 doc=texts[paper_ID]
 vec_bow = dictionary.doc2bow(doc)
 doc_lsi = lsi[vec_bow]
-
 sims = index[doc_lsi]
 sort_sims = sorted(enumerate(sims),key=lambda item:-item[1])
-
+# write the result
 f = open('Recommend4Paper_'+PaperID[paper_ID]+'.txt', 'w')
-
 for i in range (0,500):
 	f.write( str(sort_sims[i][1])+","+str(PaperID[sort_sims[i][0]])+"\n")
-	
 f.close()
 
 
